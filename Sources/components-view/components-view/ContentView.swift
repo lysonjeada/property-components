861b860065178a889214d511f18543d0d38fcad3
lys <lyscalefi@gmail.com>
2024-07-31 21:32:01 -0300

    add initial structure with spm

---
+++ Sources/components-view/components-view/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  components-view
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 31/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sources/components-view/components-view/components_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  components_viewApp.swift
+//  components-view
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct components_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
