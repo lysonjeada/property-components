8c655940472493ab838c52c01e412b5343d7049b
lys <lyscalefi@gmail.com>
2024-08-01 23:08:43 -0300

    package update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Components",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Components",
+            targets: ["Components"]),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
